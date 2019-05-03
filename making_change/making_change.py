90a981e2d2f92dfcc95dc5d50f63728181e81886
KROHNICK <business.kanekperez@gmail.com>
2019-05-02 18:31:33 -0700

    finished making chnage

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,23 @@
 
 import sys
 
+
 def making_change(amount, denominations):
-  pass 
+    ways_to_get_amount = [0]*(amount+1)
+    ways_to_get_amount[0] = 1
+    for denomination in denominations:
+        for i in range(denomination, amount+1):
+            ways_to_get_amount[i] += ways_to_get_amount[i-denomination]
+    return ways_to_get_amount[amount]
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
